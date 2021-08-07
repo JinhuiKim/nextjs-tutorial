8ae06b42b58953fae1984868a974b7b2886de0ed
JinhuiKim <public.jinhui.kim@gmail.com>
2021-08-08 07:15:52 +0900

    Next js 강좌 #2 Dynamic Routes, next/link - 화장품 api 호출 + axios 설치: npm i axios - 컴포넌트 추가, css 적용 + ItemList.js, Item.js + ItemList.module.css, Item.module.css - Dynamic Routes - Link

--- pages/view/[id].js
+++ pages/view/[id].js
@@ -1,3 +1,31 @@
-export default function ID() {
-  return <div>ID</div>;
-}
+import Axios from "axios";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Item from "../../src/component/Item";
+
+const Post = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const [item, setItem] = useState({});
+
+  const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+
+  function getData() {
+    Axios.get(API_URL).then((res) => {
+      setItem(res.data);
+    });
+  }
+
+  // 최초 한 번만 로드
+  useEffect(() => {
+    // id가 undefined인 경우 있어서 에러발생
+    if (id && id > 0) {
+      getData();
+    }
+  }, [id]);
+
+  return <Item item={item} />;
+};
+
+export default Post;
